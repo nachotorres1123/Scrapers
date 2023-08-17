033030640d42f686b9233685d9f9f52a4a49a19e
nacho_torres_1123@hotmail.com <43484201+nachotorres1123@users.noreply.github.com>
2023-08-17 04:13:52 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+# Configuración del bot
+TOKEN = 'YOUR_BOT_TOKEN'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# Configuración de logging
+logging.basicConfig(level=logging.INFO)
+
+# Función para extraer datos de la página web
+def extract_data():
+    url = 'https://example.com'  # Cambia esta URL a la página de la que deseas extraer datos
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Aquí selecciona los elementos que deseas extraer usando métodos de BeautifulSoup
+
+        # Ejemplo: Extraer el texto de todos los elementos <h1> en la página
+        data = [element.text for element in soup.find_all('h1')]
+
+        return data
+    else:
+        return None
+
+# Manejador del comando /start
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("¡Hola! Soy un bot para extraer datos de una página web y mostrarlos en Telegram.")
+
+# Manejador del comando /getdata
+@dp.message_handler(commands=['getdata'])
+async def get_data(message: types.Message):
+    await message.answer_chat_action('typing')
+
+    extracted_data = extract_data()
+    if extracted_data:
+        data_message = '\n'.join(extracted_data)
+        await message.reply(data_message)
+    else:
+        await message.reply('No se pudo obtener los datos.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
